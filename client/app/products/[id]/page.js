ffc2817b871cedcaebbb9f15861a75ee2ee54b57
AdityaBarad5090 <adityasinhbarad@gmail.com>
2026-07-31 15:58:29 +0530

    Function Changes

--- client/app/products/[id]/page.js
+++ client/app/products/[id]/page.js
@@ -3,18 +3,20 @@ import ProductDetails from "./productdetails";
 const API = `${process.env.NEXT_PUBLIC_API_URL}/products`;
 
 async function getProduct(id) {
-    const res = await fetch(`${API}/${id}`, {
-        cache: "no-store",
-    });
+    const res = await fetch(`${API}/${id}`, { cache: "no-store" });
+
+    if (!res.ok) {
+        console.error(`Failed to fetch product ${id}, status: ${res.status}`);
+        return null;
+    }
 
     const data = await res.json();
-
     return data.product;
 }
 
 export async function generateMetadata({ params }) {
     const { id } = await params;
-        
+
     const product = await getProduct(id);
 
     if (!product) {
