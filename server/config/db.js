8e4fe05d77d03fb8e7181dc49dd027b44c511b5f
AdityaBarad5090 <adityasinhbarad@gmail.com>
2026-07-31 15:10:42 +0530

    fix: use getConnection for pool instead of connect

--- server/config/db.js
+++ server/config/db.js
@@ -5,19 +5,22 @@ import mysql from "mysql2";
 
 const db = mysql.createPool({
   host: process.env.DB_HOST,
-  port:process.env.DB_PORT,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD, 
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
-db.connect((err) => {
+db.getConnection((err, connection) => {
   if (err) {
     console.error("DB Error:", err);
-    return;  
+    return;
   }
-  
   console.log("DB Connected");
+  connection.release();
 });
 
 export default db;
