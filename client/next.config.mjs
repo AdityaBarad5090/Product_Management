f626bd60289b6a149e3c2775a4eb25214f892db3
AdityaBarad5090 <adityasinhbarad@gmail.com>
2026-08-03 11:29:28 +0530

    Add an pwa

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,17 +1,30 @@
+import withPWA from "next-pwa";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     unoptimized: true,
     remotePatterns: [
       {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '5000',
-        pathname: '/uploads/**',
+        protocol: "http",
+        hostname: "localhost",
+        port: "5000",
+        pathname: "/uploads/**",
+      },
+      {
+        protocol: "https",
+        hostname: "product-server-zw3b.onrender.com",
+        pathname: "/uploads/**",
       },
     ],
   },
-  
 };
 
-export default nextConfig;
+const pwaConfig = withPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
+
+export default pwaConfig(nextConfig);
